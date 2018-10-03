a0ad70e4708d809b3fb6338f5ae30120f5c2b2e3
Jarko Umbrelluck <35334179+umbrelluck@users.noreply.github.com>
2018-10-03 20:07:44 +0300

    Add files via upload

---
+++ Gray_code.py
@@ -0,0 +1,42 @@
+def makeCode(item):
+    """
+    item - iterable or positive integer
+     returns iterable or positive integer or string if input not valid
+    """
+    if type(item) == list:
+        codded_list = []
+        for elem in item:
+            codded_list.append(elem ^ (elem >> 1))
+        return codded_list
+    elif type(item) == int:
+        return item ^ (item >> 1)
+    else:
+        return 'Not valid input'
+
+
+def deCode(item):
+    """
+    item - iterable or positive integer
+     returns iterable or positive integer or string if input not valid
+    """
+    if type(item) == list:
+        decodded_list = []
+        for elem in item:
+            res, elem_copy = elem, elem >> 1
+            while elem_copy != 0:
+                res ^= elem_copy
+                elem_copy >>= 1
+            decodded_list.append(res)
+        return decodded_list
+    elif type(item) == int:
+        res, elem_copy = item, item >> 1
+        while elem_copy != 0:
+            res ^= elem_copy
+            elem_copy >>= 1
+        return res
+    else:
+        return 'Not valid input'
+
+
+code = makeCode('jkbsbjhdb')
+print(deCode(code))
